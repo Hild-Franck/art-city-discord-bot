b028f23d9390c16e49498f8856ea9380d05b248a
Franck <franck.hild@gmail.com>
2026-01-02 01:10:32 -0500

    style: apply prettier rules on report command

--- src/commands/context/reportMessage.js
+++ src/commands/context/reportMessage.js
@@ -2,8 +2,8 @@ import {
 	ContextMenuCommandBuilder,
 	ApplicationCommandType,
 	MessageFlags,
-    EmbedBuilder,
-    ActionRowBuilder,
+	EmbedBuilder,
+	ActionRowBuilder,
 	ButtonBuilder,
 	ButtonStyle
 } from "discord.js"
@@ -16,69 +16,71 @@ export const data = new ContextMenuCommandBuilder()
 
 export const execute = async interaction => {
 	const message = await interaction.channel.messages.fetch(interaction.targetId)
-    const images = [...message.attachments.values()].filter(att =>
-        att.contentType?.startsWith("image/")
-    )
-    const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-            .setCustomId("reportConfirm")
-            .setLabel("Confirmer")
-            .setStyle(ButtonStyle.Danger),
-        new ButtonBuilder()
-            .setCustomId("reportCancel")
-            .setLabel("Annuler")
-            .setStyle(ButtonStyle.Secondary)
-    )
-    const embedLogs = new EmbedBuilder()
-        .setTitle(":rotating_light: Nouveau message signalé !")
-        .setColor(0xe74c3c)
-        .setDescription(
-            `${roles.get("mods")}, un signalement requiert votre attention !`)
-        .addFields(
-            {
-                name:"Auteur",
-                value: message.author.toString(),
-                inline: true
-            },
-            {
-                name: "Signalé par",
-                value: interaction.user.toString(),
-                inline: true
-            },
-            {
-                name: "Message",
-                value: `[Aller au message](${message.url})`,
-                inline: true
-            }
-
-        )
-
-    const embedConfirm = new EmbedBuilder()
-	.setColor(0xf1c40f)
-	.setTitle("⚠️ Demande de confirmation de signalement")
-	.setDescription(
-		`${interaction.user}, es-tu sûr(e) de vouloir signaler ce message ?\n` +
-		"*Les signalements abusifs peuvent entraîner des sanctions.*"
+	const images = [...message.attachments.values()].filter(att =>
+		att.contentType?.startsWith("image/")
 	)
-    const embedCancel = new EmbedBuilder()
-	.setColor(0x95a5a6)
-	.setDescription("❌ Signalement annulé.")
+	const row = new ActionRowBuilder().addComponents(
+		new ButtonBuilder()
+			.setCustomId("reportConfirm")
+			.setLabel("Confirmer")
+			.setStyle(ButtonStyle.Danger),
+		new ButtonBuilder()
+			.setCustomId("reportCancel")
+			.setLabel("Annuler")
+			.setStyle(ButtonStyle.Secondary)
+	)
+	const embedLogs = new EmbedBuilder()
+		.setTitle(":rotating_light: Nouveau message signalé !")
+		.setColor(0xe74c3c)
+		.setDescription(
+			`${roles.get("mods")}, un signalement requiert votre attention !`
+		)
+		.addFields(
+			{
+				name: "Auteur",
+				value: message.author.toString(),
+				inline: true
+			},
+			{
+				name: "Signalé par",
+				value: interaction.user.toString(),
+				inline: true
+			},
+			{
+				name: "Message",
+				value: `[Aller au message](${message.url})`,
+				inline: true
+			}
+		)
 
-    const embedSuccess = new EmbedBuilder()
-	.setColor(0x2ecc71)
-	.setDescription("✅ Merci, ton signalement a bien été transmis aux modérateurs.")
+	const embedConfirm = new EmbedBuilder()
+		.setColor(0xf1c40f)
+		.setTitle("⚠️ Demande de confirmation de signalement")
+		.setDescription(
+			`${interaction.user}, es-tu sûr(e) de vouloir signaler ce message ?\n` +
+				"*Les signalements abusifs peuvent entraîner des sanctions.*"
+		)
+	const embedCancel = new EmbedBuilder()
+		.setColor(0x95a5a6)
+		.setDescription("❌ Signalement annulé.")
 
-    const embedTimeout = new EmbedBuilder()
-	.setColor(0x95a5a6)
-	.setDescription("❌ Signalement annulé (délai dépassé).")
+	const embedSuccess = new EmbedBuilder()
+		.setColor(0x2ecc71)
+		.setDescription(
+			"✅ Merci, ton signalement a bien été transmis aux modérateurs."
+		)
+
+	const embedTimeout = new EmbedBuilder()
+		.setColor(0x95a5a6)
+		.setDescription("❌ Signalement annulé (délai dépassé).")
 
 	await interaction.reply({
 		embeds: [embedConfirm],
-        components: [row],
+		components: [row],
 		flags: MessageFlags.Ephemeral
 	})
-    const replyMessage = await interaction.fetchReply()
-    let buttonInteraction
+	const replyMessage = await interaction.fetchReply()
+	let buttonInteraction
 	try {
 		buttonInteraction = await replyMessage.awaitMessageComponent({
 			filter: i =>
@@ -86,27 +88,25 @@ export const execute = async interaction => {
 				(i.customId === "reportConfirm" || i.customId === "reportCancel"),
 			time: 15_000
 		})
-    } catch {
-        return interaction.editReply({
-		    embeds: [embedTimeout],
+	} catch {
+		return interaction.editReply({
+			embeds: [embedTimeout],
 			components: []
 		})
 	}
-    if (buttonInteraction.customId === "reportCancel") {
+	if (buttonInteraction.customId === "reportCancel") {
 		return buttonInteraction.update({
 			embeds: [embedCancel],
 			components: []
 		})
 	}
-    if (buttonInteraction.customId === "reportConfirm") {
-	await buttonInteraction.update({
-		embeds: [embedSuccess],
-		components: []
-	})
-    await channels
-		.get("logs")
-		.send({ embeds: [embedLogs] })
+	if (buttonInteraction.customId === "reportConfirm") {
+		await buttonInteraction.update({
+			embeds: [embedSuccess],
+			components: []
+		})
+		await channels.get("logs").send({ embeds: [embedLogs] })
 
-	return
-}
+		return
+	}
 }
